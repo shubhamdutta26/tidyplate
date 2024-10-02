53164d6684b7e8e3011663f3c71cc5b40af70920
Shubham Dutta <shubhamdutta26@protonmail.com>
2024-10-02 18:34:44 -0400

    updated view_plate_names and associated tests

--- R/view_plate_names.R
+++ R/view_plate_names.R
@@ -15,134 +15,54 @@
 #'
 #' data_12
 view_plate_names <- function(file, sheet = 1){
-  # check if one file is provided by the user
-  if (typeof(file) %in% c("double", "integer", "logical")) {
+
+  # Set default well_id value
+  well_id = "well"
+
+  # Check whether function arguments are valid----
+  ## One file should be provided----
+  if (length(file) != 1) {
     stop(
-      paste0("file argument cannot be of type: ", typeof(file)),
-      call. = FALSE
-    )
-  } else if (length(file) > 1) {
-    stop(
-      paste0("More than one file provided. Only one file should be provided"),
+      paste0(
+        "Invalid input: ",
+        ifelse(length(file) > 1,
+               "More than one file provided."
+        )
+      ),
       call. = FALSE
     )
   }
 
-  # Extract file name
-  # file_name <- sub("(.*\\/)([^.]+)(\\.[[:alnum:]]+$)", "\\2", file)
-  file_ext <- tools::file_ext(file)
+  ## Read file ext and basename----
+  file_ext <- tolower(tools::file_ext(file))
   file_full_name <- basename(file)
 
-  # Check if file exists
+  ## Check if file exists----
   if (!(file.exists(file))) {
-    stop(paste0(file_full_name, ": Not OK; File does not exist."), call. = FALSE)
+    stop(paste0(file_full_name, " does not exist!"), call. = FALSE)
   }
 
-  # Determine file ext and call appropriate function to import as raw_data
-  if (!(file_ext %in% c("xlsx", "csv"))) {
-    stop(paste0(file_full_name, ": Not OK; Must be either xlsx or csv file."), call. = FALSE)
-  } else if (tools::file_ext(file) == "xlsx") {
-    suppressMessages(raw_data <- readxl::read_excel(file, col_names = FALSE, sheet = sheet))
-  } else {
-    suppressMessages(raw_data <- readr::read_csv(file, col_names = FALSE, show_col_types = FALSE))
-  }
-
-  # Check if file is empty
-  # xlsx files cannot be empty
-  if (nrow(raw_data) == 0) {
-    stop(paste0(file_full_name, ": Not OK; File is empty."), call. = FALSE)
-  }
+  # Read data----
+  raw_data <- read_data(file = file, file_ext = file_ext, sheet = sheet)
 
   # Count number of columns and rows in raw_data
   count_columns <- ncol(raw_data)
   count_rows_actual <- nrow(raw_data)
 
-  # Check if there are a exact number of columns
-  if (!(count_columns %in% c(4L, 5L, 7L, 9L, 13L, 25L, 49L))) {
-    stop(paste0(file_full_name, ": Not OK; Verify input data format."), call. = FALSE)
-  }
+  # Check if plate format is valid----
+  plate_parameters <- valid_plate(raw_data,
+                                  count_columns ,
+                                  count_rows_actual,
+                                  well_id,
+                                  file_full_name)[[2]]
 
-  # This counts the number of plates in the dataset
-  # This counts the theoretical number of rows the raw_data should have for each plate type
-  # This determines the plate type
-  # Initialize indexing for each plate size
-  # Sets increment value for counter for each well type
-  row_start <- 1L
-  plate_parameters <- plate_params(raw_data, count_columns)
+  plate_names <- unlist(lapply(plate_parameters, function(x) x[1,1]),
+                        use.names = FALSE)
 
-  # This checks whether the input formating is correct or not.
-  # There must be one empty row between each data.
-  if (plate_parameters[[2]] != count_rows_actual) {
-    stop(paste0(file_full_name, ": Not OK; Verify input data format."),
-         call. = FALSE
-    )
-  }
+  list_of_plates <- lapply(plate_parameters, convert_first_row_to_header)
 
-  # Initialize empty list to store the raw_data.
-  # Each element in the list will store one plate from the raw_data
-  # no_of_plates <- as.integer(plate_parameters[[1]])
-  list_of_plates <- vector(mode = "list", length = plate_parameters[[1]])
 
-  # This is a vector which will be used to name each plate
-  # name_of_plates <- paste0("plate", 1:plate_parameters[[1]])
+  selected_plates <- extract_plates(list = list_of_plates)
 
-  # Initialize counter for indexing each plate from raw_data
-  counter <- 0
-
-  # Store each plate as an obect in a list
-  for (i in 1:plate_parameters[[1]]) {
-    list_of_plates[[i]] <- raw_data[(row_start + counter):(plate_parameters[[5]] + counter), ]
-    counter <- counter + plate_parameters[[6]]
-  }
-
-  # Make the list a named list
-  # names(list_of_plates) <- name_of_plates
-
-  # Extract names for each plate
-  each_plate_name <- purrr::map(list_of_plates, function(x) {
-    x |>
-      dplyr::slice(1) |>
-      dplyr::select(1)
-  }) |>
-    unlist(use.names = FALSE)
-
-  # Check if plate name exists and unique
-  if (sum(is.na(each_plate_name)) != 0L || length(each_plate_name) != length(unique(each_plate_name))) {
-    stop(paste0(file_full_name, ": Not OK; Verify each plate has a unique name."), call. = FALSE)
-  }
-
-  # Check if plates have 1:x as column names after plate name
-  first_row <- purrr::map(list_of_plates, function(x) {
-    x |>
-      dplyr::slice(1) |>
-      dplyr::select(-1)
-  }) |>
-    purrr::map(function(x) sum(x == plate_parameters[[8]])) |>
-    purrr::map(function(x) x != (count_columns - 1)) |>
-    unlist(use.names = FALSE)
-  first_row_sum <- replace(first_row, is.na(first_row), TRUE) |>
-    sum() # has to be 0; otherwise there is at least one plate that has bad column names
-
-  # Check if plates have A:x as row names after plate name
-  first_col <- purrr::map(list_of_plates, function(x) x |> dplyr::select(1)) |>
-    purrr::map(function(x) {
-      x |>
-        dplyr::filter(dplyr::row_number() != 1) |>
-        t() |>
-        toupper()
-    }) |>
-    purrr::map(function(x) sum(x != plate_parameters[[7]])) |>
-    unlist(use.names = FALSE)
-  first_col_sum <- replace(first_col, is.na(first_col), TRUE) |>
-    sum() # has to be 0; otherwise there is at least one plate that has bad row names
-
-  if (first_row_sum != 0L & first_col_sum != 0L) {
-    stop(paste0(file_full_name, ": Not OK; Verify row and column names."), call. = FALSE)
-  } else if (first_row_sum != 0L) {
-    stop(paste0(file_full_name, ": Not OK; Verify column names."), call. = FALSE)
-  } else if (first_col_sum != 0L) {
-    stop(paste0(file_full_name, ": Not OK; Verify row names."), call. = FALSE)
-  }
-  message(paste0(file_full_name, ": OK; Plate type: ", plate_parameters[[3]], " well"))
-  return(each_plate_name)
+  return(plate_names)
 }
--- tests/testthat/test-view_plate_names.R
+++ tests/testthat/test-view_plate_names.R
@@ -1,193 +1,19 @@
 for (i in c(6, 12, 24, 48, 96, 384, 1536)) {
   path <- paste0("test_data/", i, "/")
 
-  test_that("view_plate_names works for complete valid data", {
-    csv_file <- paste0(path, "allWellIds.csv")
-    xlsx_file <- paste0(path, "allWellIds.xlsx")
-
-    result_csv <- view_plate_names(csv_file)
-    result_xlsx <- view_plate_names(xlsx_file)
-
-    expect_identical(result_csv, "values")
-    expect_identical(result_xlsx, "values")
-  })
-
-  test_that("view_plate_names works for missing data from plate", {
-    csv_file <- paste0(path, "wellIdsAndEmptyWells.csv")
-    xlsx_file <- paste0(path, "wellIdsAndEmptyWells.xlsx")
-
-    result_csv <- view_plate_names(csv_file)
-    result_xlsx <- view_plate_names(xlsx_file)
-
-    expect_identical(result_csv, "values")
-    expect_identical(result_xlsx, "values")
-  })
-
-  test_that("view_plate_names works with one full plate and one partially empty", {
+  test_that("view_plate_names extracts the correct names", {
     csv_file <- paste0(path, "oneFullOnePartEmpty.csv")
     xlsx_file <- paste0(path, "oneFullOnePartEmpty.xlsx")
 
     result_csv <- view_plate_names(csv_file)
     result_xlsx <- view_plate_names(xlsx_file)
 
-    expect_identical(result_csv, c("full", "partial"))
-    expect_identical(result_xlsx, c("full", "partial"))
-  })
+    expected_output <- c("full", "partial")
 
-  test_that("view_plate_names returns a character vector", {
-    expect_type(view_plate_names(paste0(path, "allWellIds.csv")), "character")
-    expect_type(view_plate_names(paste0(path, "allWellIds.xlsx")), "character")
-  })
-
-  test_that("view_plate_names allows weird characters", {
-    csv_file <- paste0(path, "weirdCharacters.csv")
-    xlsx_file <- paste0(path, "weirdCharacters.xlsx")
-
-    result_csv <- view_plate_names(csv_file)
-    result_xlsx <- view_plate_names(xlsx_file)
-
-    expect_identical(result_csv, "values")
-    expect_identical(result_xlsx, "values")
-  })
-
-  test_that("view_plate_names works with single well", {
-    csv_file <- paste0(path, "oneWell.csv")
-    xlsx_file <- paste0(path, "oneWell.xlsx")
-
-    result_csv <- view_plate_names(csv_file)
-    result_xlsx <- view_plate_names(xlsx_file)
-
-    expect_identical(result_csv, "values")
-    expect_identical(result_xlsx, "values")
-  })
-
-  test_that("view_plate_names throws error when plate format is not correct", {
-    csv_file_1 <- paste0(path, "extraCol.csv")
-    csv_file_2 <- paste0(path, "twoPlatesExtraCol.csv")
-    xlsx_file_1 <- paste0(path, "extraCol.xlsx")
-    xlsx_file_2 <- paste0(path, "twoPlatesExtraCol.xlsx")
-
-    expect_error(
-      view_plate_names(csv_file_1),
-      "extraCol.csv: Not OK; Verify input data format."
-    )
-    expect_error(
-      view_plate_names(csv_file_2),
-      "twoPlatesExtraCol.csv: Not OK; Verify input data format."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_1),
-      "extraCol.xlsx: Not OK; Verify input data format."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_2),
-      "twoPlatesExtraCol.xlsx: Not OK; Verify input data format."
-    )
-  })
-
-
-  test_that("view_plate_names throws error when plate names are either empty or not unique", {
-    csv_file_1 <- paste0(path, "sameNames.csv")
-    xlsx_file_1 <- paste0(path, "sameNames.xlsx")
-    csv_file_2 <- paste0(path, "noName.csv")
-    xlsx_file_2 <- paste0(path, "noName.xlsx")
-
-    expect_error(
-      view_plate_names(csv_file_1),
-      "sameNames.csv: Not OK; Verify each plate has a unique name."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_1),
-      "sameNames.xlsx: Not OK; Verify each plate has a unique name."
-    )
-    expect_error(
-      view_plate_names(csv_file_2),
-      "noName.csv: Not OK; Verify each plate has a unique name."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_2),
-      "noName.xlsx: Not OK; Verify each plate has a unique name."
-    )
-  })
-
-  test_that("view_plate_names throws error when row and column ids are empty or bad", {
-    csv_file_1 <- paste0(path, "badColRowName.csv")
-    xlsx_file_1 <- paste0(path, "badColRowName.xlsx")
-    csv_file_2 <- paste0(path, "emptyColRowName.csv")
-    xlsx_file_2 <- paste0(path, "emptyColRowName.xlsx")
-
-    expect_error(
-      view_plate_names(csv_file_1),
-      "badColRowName.csv: Not OK; Verify row and column names."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_1),
-      "badColRowName.xlsx: Not OK; Verify row and column names."
-    )
-    expect_error(
-      view_plate_names(csv_file_2),
-      "emptyColRowName.csv: Not OK; Verify row and column names."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_2),
-      "emptyColRowName.xlsx: Not OK; Verify row and column names."
-    )
-  })
-
-  test_that("view_plate_names throws error when row id(s) are empty or bad", {
-    csv_file_1 <- paste0(path, "badRowName.csv")
-    xlsx_file_1 <- paste0(path, "badRowName.xlsx")
-    csv_file_2 <- paste0(path, "emptyRowName.csv")
-    xlsx_file_2 <- paste0(path, "emptyRowName.xlsx")
-
-    expect_error(
-      view_plate_names(csv_file_1),
-      "badRowName.csv: Not OK; Verify row names."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_1),
-      "badRowName.xlsx: Not OK; Verify row names."
-    )
-    expect_error(
-      view_plate_names(csv_file_2),
-      "emptyRowName.csv: Not OK; Verify row names."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_2),
-      "emptyRowName.xlsx: Not OK; Verify row names."
-    )
-  })
-
-  test_that("view_plate_names throws error when column id(s) are empty or bad", {
-    csv_file_1 <- paste0(path, "badColName.csv")
-    xlsx_file_1 <- paste0(path, "badColName.xlsx")
-    csv_file_2 <- paste0(path, "emptyColName.csv")
-    xlsx_file_2 <- paste0(path, "emptyColName.xlsx")
-
-    expect_error(
-      view_plate_names(csv_file_1),
-      "badColName.csv: Not OK; Verify column names."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_1),
-      "badColName.xlsx: Not OK; Verify column names."
-    )
-    expect_error(
-      view_plate_names(csv_file_2),
-      "emptyColName.csv: Not OK; Verify column names."
-    )
-    expect_error(
-      view_plate_names(xlsx_file_2),
-      "emptyColName.xlsx: Not OK; Verify column names."
-    )
+    expect_identical(result_csv, expected_output)
+    expect_identical(result_xlsx, expected_output)
   })
 }
-
-test_that("view_plate_names throws an error when user inputs bad file type", {
-  expect_error(view_plate_names(file = 1), "file argument cannot be of type: double")
-  expect_error(view_plate_names(file = 1L), "file argument cannot be of type: integer")
-  expect_error(view_plate_names(file = TRUE), "file argument cannot be of type: logical")
-})
 
 test_that("view_plate_names throws an error when user inputs more than one file", {
   file_1 <- "test_data/allWellIds.csv"
@@ -195,28 +21,27 @@ test_that("view_plate_names throws an error when user inputs more than one file"
 
   expect_error(
     view_plate_names(file = c(file_1, file_2)),
-    "More than one file provided. Only one file should be provided"
+    "Invalid input: More than one file provided."
   )
 })
 
-# # Test for filenames extraction
-
+# Test for filenames extraction
 test_that("view_plate_names throws an error when input file is non existant", {
   csv_file <- "test_data/doesNotExist.csv"
   xlsx_file <- "test_data/doesNotExist.xlsx"
 
-  expect_error(view_plate_names(csv_file), "doesNotExist.csv: Not OK; File does not exist.")
-  expect_error(view_plate_names(xlsx_file), "doesNotExist.xlsx: Not OK; File does not exist.")
+  expect_error(view_plate_names(csv_file), "doesNotExist.csv does not exist!")
+  expect_error(view_plate_names(xlsx_file), "doesNotExist.xlsx does not exist!")
 })
 
 test_that("view_plate_names throws an error when input file is not csv or xlsx", {
   file <- "test_data/badFileType.txt"
 
-  expect_error(view_plate_names(file), "badFileType.txt: Not OK; Must be either xlsx or csv file.")
+  expect_error(view_plate_names(file), "Unsupported file format. Please use CSV or Excel files.")
 })
 
 test_that("view_plate_names throws an error when input file is empty", {
   csv_file <- "test_data/emptyFile.csv"
 
-  expect_error(view_plate_names(csv_file), "emptyFile.csv: Not OK; File is empty.")
+  expect_error(view_plate_names(csv_file), "The input file or sheet is empty.")
 })
