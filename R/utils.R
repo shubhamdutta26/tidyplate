2785675b28ae0c2a5855a9ec79f1fa3cb9d45fb3
Shubham Dutta <shubhamdutta26@protonmail.com>
2024-10-01 20:18:13 -0400

    added plate_dims function

--- R/utils.R
+++ R/utils.R
@@ -108,6 +108,17 @@ valid_plate <- function(raw_data,
   return(list(plate_parameters, list_of_plates))  # Return the processed plates
 }
 
+# Define the number of rows and columns for each plate type
+plate_dims <- list(
+  `6` = c(2, 3),
+  `12` = c(3, 4),
+  `24` = c(4, 6),
+  `48` = c(6, 8),
+  `96` = c(8, 12),
+  `384` = c(16, 24),
+  `1536` = c(32, 48)
+)
+
 
 plate_params <- function(data, n_cols) {
   # Define a list of parameters for each n_cols value
