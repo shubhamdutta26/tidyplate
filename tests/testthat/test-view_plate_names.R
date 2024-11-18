4f98ded36171733d0fb2792b1f7aa2a91d5efded
Shubham Dutta <shubhamdutta26@protonmail.com>
2024-11-18 13:12:05 -0500

    adding tests to view_plate_names function

--- tests/testthat/test-view_plate_names.R
+++ tests/testthat/test-view_plate_names.R
@@ -13,6 +13,46 @@ for (i in c(6, 12, 24, 48, 96, 384, 1536)) {
     expect_identical(result_csv, expected_output)
     expect_identical(result_xlsx, expected_output)
   })
+
+  test_that("view_plate_names throws error when plate format is not correct", {
+    csv_file_1 <- paste0(path, "extraCol.csv")
+    csv_file_2 <- paste0(path, "twoPlatesExtraCol.csv")
+    xlsx_file_1 <- paste0(path, "extraCol.xlsx")
+    xlsx_file_2 <- paste0(path, "twoPlatesExtraCol.xlsx")
+
+    expect_error(
+      view_plate_names(csv_file_1),
+      regexp = "is not a valid input file"
+    )
+    expect_error(
+      view_plate_names(csv_file_2),
+      regexp = "is not a valid input file"
+    )
+    expect_error(
+      view_plate_names(xlsx_file_1),
+      regexp = "is not a valid input file"
+    )
+    expect_error(
+      view_plate_names(xlsx_file_2),
+      regexp = "is not a valid input file"
+    )
+  })
+
+  test_that("view_plate_names message if no plate names", {
+    csv_file <- paste0(path, "noName.csv")
+    xlsx_file <- paste0(path, "noName.xlsx")
+
+    expect_message(view_plate_names(csv_file), regexp = "Empty ")
+    expect_message(view_plate_names(xlsx_file), regexp = "Empty ")
+  })
+
+  test_that("view_plate_names message if duplicated plate names", {
+    csv_file <- paste0(path, "sameNames.csv")
+    xlsx_file <- paste0(path, "sameNames.xlsx")
+
+    expect_message(view_plate_names(csv_file), regexp = "Duplicated plate ")
+    expect_message(view_plate_names(xlsx_file), regexp = "Duplicated plate ")
+  })
 }
 
 test_that("view_plate_names throws an error when user inputs more than one file", {
@@ -48,4 +88,29 @@ test_that("view_plate_names throws an error when input file is empty", {
 
   expect_error(view_plate_names(csv_file),
                regexp = "file is empty")
+})
+
+
+test_that("view_plate_names throws error when plate format is not correct", {
+  csv_file <- "test_data/extraRow.csv"
+  xlsx_file <- "test_data/extraRow.xlsx"
+
+  expect_error(
+    view_plate_names(csv_file),
+    regexp = "is not a valid input file"
+  )
+  expect_error(
+    view_plate_names(xlsx_file),
+    regexp = "is not a valid input file"
+  )
+})
+
+test_that("view_plate_names error when no name and duplicates", {
+  csv_file <- "test_data/noNameDup.csv"
+  xlsx_file <- "test_data/noNameDup.xlsx"
+
+  expect_error(view_plate_names(csv_file),
+               regexp = "& duplicate plate names found in")
+  expect_error(view_plate_names(xlsx_file),
+               regexp = "& duplicate plate names found in")
 })
